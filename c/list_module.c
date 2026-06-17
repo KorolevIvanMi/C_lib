e46a72389aaf6b2ddaed5296c230b2b470ba0379
KorolevIvanMi <Ivankorol1512@yandex.ru>
2026-06-17 21:53:10 +0300

    add delete_module

--- c/list_module.c
+++ c/list_module.c
@@ -9,6 +9,8 @@
 #include <core_module.h>
 #include <read_module.h>
 #include <update_module.h>
+#include <delete_module.h>
+
 
 // description of class fields
 static PyMemberDef MyList_members[] = {
@@ -20,14 +22,6 @@ static PyMemberDef MyList_members[] = {
 // method of adding to the end of a list
 static PyObject*
 append(PyObject* op, PyObject* args);
-
-
-
-// method of deleting last element
-static PyObject*
-pop(PyObject* op, PyObject* Py_UNUSED(dummy));
-
-
 
 // description of added methods
 static PyMethodDef MyList_methods[] = {
@@ -120,29 +114,5 @@ append(PyObject* op, PyObject* args){
     }
     current->next = new_element;
     Py_RETURN_NONE;
-}
-
-
-static PyObject*
-pop(PyObject* op, PyObject* Py_UNUSED(dummy)){
-    MyList* self = (MyList*) op;
-    if(self == NULL){
-        Py_RETURN_NONE;
-    }
-    if(self->next==NULL){
-        PyObject* value = self->value;
-        self->value = NULL;
-        return value;
-    }
-    MyList* current = self;
-    while(current->next->next != NULL){
-        current = current->next;
-    }
-    PyObject* value = current->next->value;
-    current->next->value = NULL;
-    Py_TYPE(current->next)->tp_free(current->next);
-    current->next = NULL;
-    
-    return value;
 }
 
---
+++ headers/delete_module.h
@@ -0,0 +1,9 @@
+//this module contains delete-like functions
+#ifndef DELETE_MODULE_H
+#define DELETE_MODULE_H
+
+// method of deleting last element
+PyObject*
+pop(PyObject* op, PyObject* Py_UNUSED(dummy));
+
+#endif
